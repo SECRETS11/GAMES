6f60703d154a6fc9d8c60f6ddd4127ba256784fe
SHIVAMCODESKING1 <92265003+SHIVAMCODESKING1@users.noreply.github.com>
2023-11-07 23:02:55 +0530

    Add files via upload

---
+++ MR_MM/mapper_MM.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#https://github.com/amberm291/MatrixMultiplyMR/blob/master/README.md
+#add xrange in case of using a older version of python
+import sys
+
+row_a, col_b = 5,5
+
+for line in sys.stdin:
+	matrix_index, row, col, value = line.rstrip().split(",")
+	if matrix_index == "A":
+		for i in range(0,col_b):
+			key = row + "," + str(i)
+			print("%s\t%s\t%s"%(key,col,value))
+	else:
+		for j in range(0,row_a):
+			key = str(j) + "," + col 
+			print("%s\t%s\t%s"%(key,row,value))
---
+++ SocialAnalysis.R
@@ -0,0 +1,14 @@
+
+install.packages("igraph") #important for colab
+
+library(igraph)
+
+edges <- data.frame(form=c("Alice", "Bob", "Alice", "Charlie", "Dave", "Eve"),
+                   to=c("Bob", "Charlie", "Dave", "Bob", "Alice", "Eve"))
+
+
+so_net <- graph_from_data_frame(edges)
+
+plot(so_net, layout=layout.fruchterman.reingold,vertex.color="blue", vertex.size=30, edge.color="gray", edege.width=1,main="Social Network Analysis")
+
+
